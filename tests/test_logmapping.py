a259a715a14abb3d949908a58d937a89f3c2a469
Stefan Petrescu <stefan.petrescu997@gmail.com>
2022-06-02 14:13:15 +0200

    Add initial tests for logmapping module

--- tests/test_logmapping.py
+++ tests/test_logmapping.py
@@ -1,1 +1,19 @@
-from logmapping import logmapping
+from logmapping.logmapping import find_str
+from collections import Counter
+
+def test_count_words_substring_not_in_string():
+    """Test word counting from a file."""
+    expected = (-1, -1)
+    string = "runt_log"
+    substring = "constant"
+    actual = find_str(string=string, substring=substring)
+    assert actual == expected, "Test ran successfully!"
+
+def test_count_words_substring_is_in_string():
+    """Test word counting from a file."""
+    expected = (0, 1)
+    
+    string = "runt_log"
+    substring = "r"
+    actual = find_str(string=string, substring=substring)
+    assert actual == expected, "Test ran successfully!"
