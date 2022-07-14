37fee9c17c08b04b923b6f794cb3131c0bf936a4
Stefan Petrescu <stefan.petrescu997@gmail.com>
2022-07-15 01:53:08 +0200

    Add utils module

---
+++ src/logmapping/utils.py
@@ -0,0 +1,15 @@
+CODE_EXTENSIONS_SPECIFIC_LANGUAGE = {
+    "JAVA": "*.java",
+    "Python": "*.py",
+    "C++": "*.cpp"
+}
+
+FORMULAE_LOGGING_STATEMENTS_SPECIFIC_LANGUAGE = {
+    "JAVA": ["LOG.info("],
+}
+
+LINE_TERMINATOR_SPECIFIC_LANGUAGE = {
+    "JAVA": ";\n",
+}
+
+RUNTIME_LOG_FILE_EXTENSION = "*.log"
